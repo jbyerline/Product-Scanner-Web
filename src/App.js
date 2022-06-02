10e562dd9d6e3920d3d0dffff45f2c97881a1e66
Jacob Byerline <jbyerline@gmail.com>
2022-06-02 14:49:06 -0700

    eslint

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from "react";
+
 import DataTable from "./components/Table/DataTable";
 import Header from "./components/Header/Header";
 import CardView from "./components/Views/CardView";
 
 export default function App() {
-
   return (
-      <div>
-          <Header/>
-          {/*<DataTable/>*/}
-          <CardView />
-      </div>
+    <div>
+      <Header />
+      {/*<DataTable/>*/}
+      <CardView />
+    </div>
   );
 }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,33 +1,32 @@
-import {AppBar, Toolbar, Tooltip} from "@mui/material";
+import { AppBar, Toolbar, Tooltip } from "@mui/material";
 import React from "react";
 import IconButton from "@mui/material/IconButton";
 import RefreshIcon from "@mui/icons-material/Refresh";
 
 export default function Header() {
+  function refreshPage() {
+    window.location.reload(false);
+  }
 
-    function refreshPage() {
-        window.location.reload(false);
-    }
-
-    return (
-        <header>
-            <AppBar position={"sticky"}>
-                <Toolbar style={{display:"flex", justifyContent:"space-between"}}>
-                    <div>
-                        <p>Product Scanner Control Center</p>
-                    </div>
-                    <div>
-                        <Tooltip
-                            title="Refresh"
-                            style={{ marginRight: "10px", marginBottom: "5px" }}
-                        >
-                            <IconButton onClick={refreshPage}>
-                                <RefreshIcon aria-label="refresh" style={{ fill: "white" }} />
-                            </IconButton>
-                        </Tooltip>
-                    </div>
-                </Toolbar>
-            </AppBar>
-        </header>
-    );
+  return (
+    <header>
+      <AppBar position="sticky">
+        <Toolbar style={{ display: "flex", justifyContent: "space-between" }}>
+          <div>
+            <p>Product Scanner Control Center</p>
+          </div>
+          <div>
+            <Tooltip
+              title="Refresh"
+              style={{ marginRight: "10px", marginBottom: "5px" }}
+            >
+              <IconButton onClick={refreshPage}>
+                <RefreshIcon aria-label="refresh" style={{ fill: "white" }} />
+              </IconButton>
+            </Tooltip>
+          </div>
+        </Toolbar>
+      </AppBar>
+    </header>
+  );
 }
