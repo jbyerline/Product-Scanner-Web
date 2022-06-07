32dee99e89d8e2dca67fdb838f5a8a22873deacd
Jacob Byerline <jbyerline@gmail.com>
2022-06-06 23:36:11 -0700

    updated table view to be styled similar to card view

--- src/App.js
+++ src/App.js
@@ -8,8 +8,8 @@ export default function App() {
   return (
     <div>
       <Header />
-      <TableView />
-      {/*<CardView />*/}
+      {/*<TableView />*/}
+      <CardView />
     </div>
   );
 }
